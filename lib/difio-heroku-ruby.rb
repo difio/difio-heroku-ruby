9ed69ebb0e97fcd5cc4e0e68c21953899b3b95b2
Alexander Todorov <atodorov@dif.io>
2012-09-29 23:40:58 +0300

    use different way to get hostname

--- lib/difio-heroku-ruby.rb
+++ lib/difio-heroku-ruby.rb
@@ -5,7 +5,7 @@ module Difio
         configure({
             'user_id'  => ENV['DIFIO_USER_ID'],
             'app_name' => ENV['DIFIO_APP_NAME'],
-            'app_uuid' => ENV['HOSTNAME'],  # this looks like an UUID
+            'app_uuid' => `hostname`.chomp,  # this looks like an UUID
             'app_type' => 'ruby-' + RUBY_VERSION,
             'app_url'  => ENV['DIFIO_APP_URL'],
             'app_vendor' => 2,  # Heroku
